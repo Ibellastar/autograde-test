43dd85dc9f908820fd387094037e6f13102e38b0
Bella Bernal <bella.bernal@atlasschool.com>
2025-04-29 13:55:39 -0500

    main file

--- src/multiplier.c
+++ src/multiplier.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
-#include "multiplier.h"
 
 int multiplier(int a, int b) {
     return a * b;
-}
-
-int main() {
-    int x = 5, y = 4;
-    int result = multiplier(x, y);
-    printf("Result: %d\n", result);
-    return 0;
 }
