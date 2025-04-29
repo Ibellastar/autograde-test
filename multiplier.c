89995d5336e5de75c71f69199e2bea634cd01b46
Bella Bernal <bella.bernal@atlasschool.com>
2025-04-29 10:48:03 -0500

    finished

--- multiplier.c
+++ multiplier.c
@@ -1,9 +1,12 @@
+/* Instructions: In the main function, write a program that multiplies two numbers together and prints the product*/
 #include <stdio.h>
 
 int main() {
+    /* You code goes here */
     int a = 5, b = 3;
     int product = a * b;
     printf("The product is %d.\n", product);
 
+    /* Return is provided and not tested for */
     return 0;
 }
